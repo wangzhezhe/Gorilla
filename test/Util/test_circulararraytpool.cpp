972a24a2224ef44900a2befeb5cea29516f82772
wangzhezhe <w_hessen@126.com>
2020-06-24 04:40:28 -0400

    updates documents

--- test/Util/test_circulararraytpool.cpp
+++ test/Util/test_circulararraytpool.cpp
@@ -2,10 +2,11 @@
 
 #include "../utils/circularArrayTPool.h"
 
-void testCoverExist() {
+void testFull() {
+  std::cout << "---testFull" << std::endl;
   try {
     CircularArrayTP carray(10);
-    for (int i = 1; i < 12; i++) {
+    for (int i = 0; i < 12; i++) {
       carray.addES();
       std::cout << "ok to add es " << i << std::endl;
     }
@@ -17,19 +18,48 @@ void testCoverExist() {
 }
 
 void testRemove() {
+  std::cout << "---testRemove" << std::endl;
+
   CircularArrayTP carray(23);
   for (int i = 0; i < 20; i++) {
     carray.addES();
-    
     carray.addES();
-    
     carray.removeES();
-    std::cout << "add two ess and remove one, tail is " << carray.m_tail << " header is " << carray.m_head << std::endl; 
+    std::cout << "add two ess and remove one, tail is " << carray.m_tail
+              << " header is " << carray.m_head << std::endl;
+  }
+
+  if (carray.esbufferLen() != 20) {
+    throw std::runtime_error("the esbufferLen is incorrect");
+  }
+}
+
+void testLen() {
+  std::cout << "---testLen" << std::endl;
+
+  CircularArrayTP carray(10);
+  if (carray.esbufferLen() != 0) {
+    throw std::runtime_error("the esbufferLen empty is incorrect");
+  }
+  carray.addES();
+  carray.addES();
+  if (carray.esbufferLen() != 2) {
+    throw std::runtime_error("the esbufferLen 2 is incorrect");
+  }
+
+  for (int i = 0; i < 6; i++) {
+    carray.addES();
+    carray.addES();
+    carray.removeES();
+  }
+  if (carray.esbufferLen() != 8) {
+    throw std::runtime_error("the esbufferLen 8 is incorrect");
   }
 }
 int main() {
   tl::abt scope;
-  testCoverExist();
+  testFull();
   testRemove();
+  testLen();
   return 0;
 }
--- utils/circularArrayTPool.h
+++ utils/circularArrayTPool.h
@@ -8,99 +8,102 @@
 namespace tl = thallium;
 
 // the thread pool based on a circular Array
+// init tail = head = 0
+// tail is empty position that do not store real element
+// head + 1 = tail full
+// tail + 1 = head the valid element is 1
+// tail = head empty
 struct CircularArrayTP {
-  CircularArrayTP(int slotNum) : m_slotNum(slotNum) {
-    if (m_slotNum < 1) {
-      throw std::runtime_error("the length of the array should larger than 1");
+  CircularArrayTP(int slotNum)
+      : m_slotNum(slotNum), m_mainES(tl::xstream::create()) {
+    if (this->m_slotNum < 1) {
+      throw std::runtime_error(
+          "the length of the es array should larger than 1");
     }
+
     // init the slots with the empty xstream
     for (int i = 0; i < slotNum; i++) {
       // the constructor of the managed element will call the default
       // constructor of the tl::xstream
-      this->m_circularArray.push_back(tl::managed<tl::xstream>());
+      this->m_circularESbuffer.push_back(tl::managed<tl::xstream>());
     }
-    // the ES with #0 could execute the task that motitor the status and adjust
-    // the size of the thread pool
-    addES();
   }
 
   ~CircularArrayTP() {
     std::cout << "CircularArrayTP is destroyed" << std::endl;
-    int size = m_circularArray.size();
+    int size = m_slotNum;
     for (int i = 0; i < size; i++) {
-      if (!m_circularArray[i]->is_null()) {
-        m_circularArray[i]->join();
+      if (!m_circularESbuffer[i]->is_null()) {
+        m_circularESbuffer[i]->join();
       }
-      // free it manually since the destroy function is private one
       // the xstream will be deleted by the managed class automatically
     }
+    this->m_mainES->join();
   }
 
-  std::vector<tl::managed<tl::xstream>> m_circularArray;
+  // this es is in charge of increasing/decreasing es in circularESbuffer
+  tl::managed<tl::xstream> m_mainES;
+  // the es in this buffer is in charge of executing the in-memory tasks
+  std::vector<tl::managed<tl::xstream>> m_circularESbuffer;
 
   tl::mutex m_indexMutex;
-  int m_head = -1;
-  int m_tail = -1;
+  int m_head = 0;
+  int m_tail = 0;
   const int m_slotNum;
   // add an actual es into the slot array
   int addES() {
-    m_indexMutex.lock();
-    bool ifInit = false;
-    if (m_head == -1 && m_tail == -1) {
-      // the start position
-      m_head = 0;
-      m_tail = 0;
-
-    } else if (this->m_head == this->m_slotNum - 1) {
-      //jump out the #0 element
-      this->m_head = 1;
-    } else {
-      this->m_head++;
+    this->m_indexMutex.lock();
+    if (((this->m_head + 1) % this->m_slotNum) == this->m_tail) {
+      // use m_head == m_tail to represent the empty
+      // use m_head+1 == m_tail to represent the full
+      // this will be convenient to caculate the length
+      this->m_indexMutex.unlock();
+      throw std::runtime_error("the es buffer is full");
     }
+    // normal move between the 0 to m_slotNum -1
 
-    if (!m_circularArray[m_head]->is_null()) {
+    this->m_head = (this->m_head + 1) % (this->m_slotNum);
+    // if the current position is not null
+    if (!this->m_circularESbuffer[this->m_head]->is_null()) {
+      this->m_indexMutex.unlock();
       throw std::runtime_error("the ess at the new position " +
-                               std::to_string(m_head) +
+                               std::to_string(this->m_head) +
                                " should be empty before moving head ptr");
     } else {
-      m_circularArray[m_head] = std::move(tl::xstream::create());
+      this->m_circularESbuffer[this->m_head] = std::move(tl::xstream::create());
     }
-
-    m_indexMutex.unlock();
+    this->m_indexMutex.unlock();
   };
 
   int removeES() {
-    if (m_head == -1 || m_tail == -1) {
-      throw std::runtime_error("fail to be initlized before removeES");
-    }
-    m_indexMutex.lock();
+    this->m_indexMutex.lock();
     int temptail = this->m_tail;
-    if (this->m_tail == this->m_slotNum - 1) {
-      //jump out the #0 element which is a master one 
-      this->m_tail = 1;
-    } else {
-      this->m_tail++;
-    }
+    this->m_tail = (this->m_tail + 1) % (this->m_slotNum);
+    this->m_indexMutex.unlock();
     // delete the es for the current position and make it empty
-    if (m_circularArray[temptail]->is_null()) {
-      throw std::runtime_error(
-          "the current slot should be an avalible es (not empty) before "
-          "deleting");
-    }
     // wait the finish of the ess
-    m_circularArray[temptail]->join();
-    // make this slot empty
-    // the previous es is deleted in the move constructor
-    m_circularArray[temptail] = std::move(tl::managed<tl::xstream>());
-    std::cout << "debug, reset for position " << temptail << std::endl;
-    m_indexMutex.unlock();
+    if (!this->m_circularESbuffer[temptail]->is_null()) {
+      this->m_circularESbuffer[temptail]->join();
+      // make this slot empty
+      // the previous es is deleted in the move constructor
+      this->m_circularESbuffer[temptail] =
+          std::move(tl::managed<tl::xstream>());
+    }
   };
 
   // get the length of the non empty es
-  int essLen() {
-    m_indexMutex.lock();
-
-    m_indexMutex.unlock();
+  int esbufferLen() {
+    int len;
+    this->m_indexMutex.lock();
+    if (this->m_head == this->m_tail) {
+      len = 0;
+    } else if (this->m_head > this->m_tail) {
+      len = this->m_head - this->m_tail;
+    } else {
+      len = this->m_head - this->m_tail + m_slotNum;
+    }
+    this->m_indexMutex.unlock();
+    return len;
   }
 };
 
